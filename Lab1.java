caf4f39ebbe363b9bcf88a3e84bb370242ba3db4
nickcarrozza25 <nicholas.carrozza1@marist.edu>
2017-01-23 21:58:47 -0500

    vim wizard

--- Lab1.java
+++ Lab1.java
@@ -1,24 +1,28 @@
-/* 
+reated by nickcarrozza on 1/23/17.
+ */
+
+
+/*
 
 Nick Carrozza
 
-CMPT 220 
+CMPT 220
 
 Lab 1
 
-*/ 
+*/
 
 
-//Problem 1.3 
+//Problem 1.3
 
 public class DriverProb1_3 {
 
-  public static void main (String[] args) {
+    public static void main (String[] args) {
 
-    System.out.println("       " + "J" + "          " + "A" + "          " + "V"   + "     " + "V" + "       "+  "A" );
-    System.out.println("       " + "J" + "        " + "A" + "   " + "A" + "         " + "V" + "   " + "V" + "      "  + "A" + "   " + "A");
-    System.out.println("J"+ " " + "     " + "J" + "        " + "AAAAA" + "          " + "V" + " " + "V" + "       " + "AAAAA");
-    System.out.println("  " + "J  J" + "         " + "A" + "     " + "A" + "          " + "V" + "    " + "   " + "A" + "     " + "A");
+        System.out.println("       " + "J" + "          " + "A" + "          " + "V"   + "     " + "V" + "       "+  "A" );
+        System.out.println("       " + "J" + "        " + "A" + "   " + "A" + "         " + "V" + "   " + "V" + "      "  + "A" + "   " + "A");
+        System.out.println("J"+ " " + "     " + "J" + "        " + "AAAAA" + "          " + "V" + " " + "V" + "       " + "AAAAA");
+        System.out.println("  " + "J  J" + "         " + "A" + "     " + "A" + "          " + "V" + "    " + "   " + "A" + "     " + "A");
 
     }
 
