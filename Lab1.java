83d24ec90427b09c61341c95dad2a2700c39572d
nickcarrozza25 <nicholas.carrozza1@marist.edu>
2017-01-23 21:55:44 -0500

    I like men

---
+++ Lab1.java
@@ -0,0 +1,25 @@
+/* 
+
+Nick Carrozza
+
+CMPT 220 
+
+Lab 1
+
+*/ 
+
+
+//Problem 1.3 
+
+public class DriverProb1_3 {
+
+  public static void main (String[] args) {
+
+    System.out.println("       " + "J" + "          " + "A" + "          " + "V"   + "     " + "V" + "       "+  "A" );
+    System.out.println("       " + "J" + "        " + "A" + "   " + "A" + "         " + "V" + "   " + "V" + "      "  + "A" + "   " + "A");
+    System.out.println("J"+ " " + "     " + "J" + "        " + "AAAAA" + "          " + "V" + " " + "V" + "       " + "AAAAA");
+    System.out.println("  " + "J  J" + "         " + "A" + "     " + "A" + "          " + "V" + "    " + "   " + "A" + "     " + "A");
+
+    }
+
+}
