31552942a289ca9765ebc17b98d54f42bbb0a140
nickcarrozza25 <nicholas.carrozza1@marist.edu>
2017-01-24 17:09:32 -0500

    changed file name to reflect public class name

--- DriverProb2_1.java
+++ DriverProb2_1.java
@@ -1,17 +1,25 @@
 /*
 
-Created by nickcarrozza on 1/23/17.
+Created by nickcarrozza on 1/24/17.
 
+*/
 
- */
-
+import java.util.Scanner;
 
 public class DriverProb2_1 {
 
-    public static void main (String[] arg){
+    public static void main(String[] args) {
 
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a value in degrees farenheit:");
+
+        double celcius;
+
+        double farenheit = (1.8 * celcius) + 32;
+
+        System.out.println(farenheit + "degrees farenheit");
 
     }
-
-
 }
