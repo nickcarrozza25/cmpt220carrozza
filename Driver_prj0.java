86a6db38e57ed7534d2dcf0ad86d55f8be8bccd2
nickcarrozza25 <nicholas.carrozza1@marist.edu>
2017-01-23 20:26:16 -0500

    fixed header

--- Driver_prj0.java
+++ Driver_prj0.java
@@ -2,15 +2,17 @@
 
 Nick Carrozza
 
-Homework 0 
+Project 0 
 
 CMPT220
 
 */ 
 
 
-public class Driver_prj0 {
+public class Driver_prj0 
+{
 
+  //Main Method
   public static void main (String[] args) {
     
     System.out.println("Hello World!");
