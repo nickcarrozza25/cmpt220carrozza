edd41249bd0743288b83c1a74d15ca60be11bc4a
nickcarrozza25 <nicholas.carrozza1@marist.edu>
2017-02-04 15:31:03 -0500

    fixed bug and spacing for prob 3.4

--- Labs/2/Prob3_4.java
+++ Labs/2/Prob3_4.java
@@ -1,5 +1,3 @@
-package Labs;
-
 /*
 
  Created by nickcarrozza on 2/4/17.
@@ -24,63 +22,63 @@ public class Prob3_4 {
 
             }
 
-            System.out.println(randNum);
-
-        } 
+        }
 
         if (randNum != 0) {
 
-                if (randNum == 1) {
+            System.out.println(randNum);
 
-                    System.out.println("January");
+            if (randNum == 1) {
 
-                } else if (randNum == 2) {
+                System.out.println("January");
 
-                    System.out.println("February");
+            } else if (randNum == 2) {
 
-                } else if (randNum == 3) {
+                System.out.println("February");
 
-                    System.out.println("March");
+            } else if (randNum == 3) {
 
-                } else if (randNum == 4) {
+                System.out.println("March");
 
-                    System.out.println("April");
+            } else if (randNum == 4) {
 
-                } else if (randNum == 5) {
+                System.out.println("April");
 
-                    System.out.println("May");
+            } else if (randNum == 5) {
 
-                } else if (randNum == 6) {
+                System.out.println("May");
 
-                    System.out.println("June");
+            } else if (randNum == 6) {
 
-                } else if (randNum == 7) {
+                System.out.println("June");
 
-                    System.out.println("July");
+            } else if (randNum == 7) {
 
-                } else if (randNum == 8) {
+                System.out.println("July");
 
-                    System.out.println("August");
+            } else if (randNum == 8) {
 
-                } else if (randNum == 9) {
+                System.out.println("August");
 
-                    System.out.println("September");
+            } else if (randNum == 9) {
 
-                } else if (randNum == 10) {
+                System.out.println("September");
 
-                    System.out.println("October");
+            } else if (randNum == 10) {
 
-                } else if (randNum == 11) {
+                System.out.println("October");
 
-                    System.out.println("November");
+            } else if (randNum == 11) {
 
-                } else if (randNum == 12) {
+                System.out.println("November");
 
-                    System.out.println("December");
-                }
+            } else if (randNum == 12) {
+
+                System.out.println("December");
 
             }
+
         }
+    }
 
 }
-
