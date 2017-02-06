7b61c36f9050ec43691fac415378890a31cb35c0
nickcarrozza25 <nicholas.carrozza1@marist.edu>
2017-02-06 13:46:54 -0500

    added changes to problem 3.11

--- Labs/Prob3_11.java
+++ Labs/Prob3_11.java
@@ -18,15 +18,21 @@ public class Prob3_11 {
 
         int monthNum = input.nextInt();
 
+        System.out.print("Please enter a four digit year value: ");
+
+        int year = input.nextInt();
+
+        int numDays = 0;
+
         String month = null;
 
         switch (monthNum) {
 
             case 1:
 
-            month = "January";
+               month = "January";
 
-            break;
+               break;
 
             case 2:
 
@@ -98,6 +104,31 @@ public class Prob3_11 {
         }
 
         System.out.println(month);
+
+        switch (month) {
+
+
+            case "January":
+
+                numDays = 31;
+
+                break;
+
+            case "February":
+
+                numDays = 28;
+
+            case "March":
+
+                numDays = 31;
+
+
+        }
+
+             System.out.println(numDays);
+
+
+
 
 
     }
