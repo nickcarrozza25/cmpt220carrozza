bb64e521ce0f7654aef055b40be2e7c6d9e3fc9e
nickcarrozza25 <nicholas.carrozza1@marist.edu>
2017-02-07 19:37:43 -0500

    finished problem 4.1 for lab 2

---
+++ Prob4_1.java
@@ -0,0 +1,34 @@
+/*
+
+ Created by nickcarrozza on 2/7/17.
+
+*/
+
+
+import java.util.Scanner;
+import java.util.Formatter;
+
+public class Prob4_1 {
+
+    public static void main (String[] args) {
+
+        System.out.print("Enter the length from the center to the vertex: ");
+
+        Scanner input = new Scanner (System.in);
+
+        double radius = input.nextDouble();
+
+        double side = (2 * radius * (Math.sin(Math.PI / 5)));
+
+        double area = ((5 * Math.pow(side, 2)) / (4 * Math.tan(Math.PI / 5)));
+
+        // to round the answer to two decimal places
+
+        double ans = (Math.round(area * 100) / 100.0);
+
+        System.out.printf("The area of the pentagon is " + ans);
+
+    }
+
+
+}
