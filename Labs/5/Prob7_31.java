09462787d53d57666a558d33ff76e9a41ffe1e91
nickcarrozza25 <nicholas.carrozza1@marist.edu>
2017-04-03 20:31:22 -0400

    finished problem 7.31 for lab 5

--- Labs/5/Prob7_31.java
+++ Labs/5/Prob7_31.java
@@ -1,70 +1,101 @@
-/**
- * Created by nickcarrozza on 3/29/17.
- */
+/*
+
+Nick Carrozza CMPT220
+
+Lab 5 - Problem 7.31
+
+Sort two user input arrays using a method
+
+*/
 
 import java.util.Scanner;
 import java.util.Arrays;
+import java.lang.Math;
+
 
 public class Prob7_31 {
 
     public static void main(String[] args){
 
-        Scanner input = new Scanner (System.in);
+        //create first instance of scanner to allow for first stream of numbers
+        Scanner firstList = new Scanner (System.in);
 
-        int[] a = new int[5];
+        System.out.print("Enter list1: ");
 
-        int[] b = new int[5];
+          int firstQty = firstList.nextInt();
 
-        for (int k = 0; k < a.length; k++){
+          int[] list1 = new int[firstQty];
 
-            int usera = input.nextInt();
+            //loop to read in the second stream of user input and create second array
+            for (int i = 0; i < list1.length; i++ ){
 
-            a[k] = usera;
-        }
+              int userFirst = firstList.nextInt();
 
-        for (int l = 0; l < b.length; l++){
+              list1[i] = userFirst;
 
-            int userb = input.nextInt();
+              }
 
-            b[l] = userb;
-        }
+          //create second instance of the scanner object to allow for second stream of input
+          Scanner secondList = new Scanner (System.in);
 
+          System.out.print("Enter list2: ");
 
-        System.out.print(Arrays.toString(a));
-        System.out.println(Arrays.toString(b));
+          int secQty = secondList.nextInt();
 
-        System.out.print(Arrays.toString(merge(a,b)));
+          int[] list2 = new int[secQty];
+
+            //loop to read in the second stream of user input and create second array
+            for (int j = 0; j < list2.length; j++ ){
+
+                int userSec = secondList.nextInt();
+
+                list2[j] = userSec;
+
+             }
+
+        //print out the result array created
+
+        System.out.print(Arrays.toString(merge(list1, list2)));
+
 
     }
 
 
-    public static int[] merge(int[] list1, int[] list2){
-
-        int[] merged = new int[list1.length + list2.length];
-
-        for (int i = 0; i < list1.length; i++){
-
-            merged[i] = list1[i];
+    //create method to take in two arrays as parameters and return the result of joining
+    //and sorting the two
+    public static int[] merge(int[] first, int[] second){
 
 
-        }
-
-        int check = 0;
-
-        for (int j = list1.length; j < merged.length; j++) {
+        //create the result array the same size as the first + second
+        int[] result = new int[first.length + second.length];
 
 
-            merged[j] = list2[check];
+          //loop to fill in the result array with values from the first array
+          for (int k = 0; k < first.length; k++){
+
+            result[k] = first[k];
+
+          }
+
+          //create a check variable to allow for the correct index of the second array to
+          //be placed in the result array
+          int check = 0;
+
+          //loop to fill in second array created to the result array, beginning where the
+          //first left off
+          for (int l = first.length; l < result.length; l++){
+
+            result[l] = second[check];
 
             check++;
 
-        }
+          }
 
-        Arrays.sort(merged);
+         //sort the result array
+         Arrays.sort(result);
 
-        return merged;
+
+        return result;
     }
 
-
 }
-
