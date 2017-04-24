28c67d036cbf1015f51657edf7d0518a1ff573c2
nickcarrozza25 <nicholas.carrozza1@marist.edu>
2017-04-24 19:17:41 -0400

    added BMI class for problem 10.2

---
+++ Labs/7/BMI.java
@@ -0,0 +1,23 @@
+/*
+
+Problem 10.2
+Create BMI constructor
+
+*/
+
+class BMI {
+
+    //default constructor for BMI class
+    BMI() {
+
+
+    }
+
+    //create new BMI constructor with the following parameters
+    public BMI(String name, int age, double weight, double feet,  double inches){
+
+    }
+
+
+
+}
