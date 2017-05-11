1df87e84206e2e7b7cb156635a7974d5d49c33d6
nickcarrozza25 <nicholas.carrozza1@marist.edu>
2017-05-11 12:09:33 -0400

    added what I was able to complete for 4.26

---
+++ Labs/2/Prob4_26.java
@@ -0,0 +1,70 @@
+/**
+ * file: Prob4_26.java
+ * author: Nick Carrozza
+ * course: CMPT 220
+ * assignment: Lab 4, Problem 4.26
+ * due date: April 27, 2017
+ * version: 1
+ *
+ * This file contains the driver for the problem 4.26 in lab
+ * 4 for a change counting program
+ */
+/**
+/**
+ * Prob4_26
+ *
+ * This class implements the driver for problem 4.26 for lab 2
+ */
+
+
+
+
+import java.util.Scanner;
+import java.lang.Math;
+
+public class Prob4_26 {
+
+    public static void main(String[] args) {
+
+      // Create a Scanner
+      Scanner input = new Scanner(System.in);
+
+      // Receive the amount
+       System.out.print("Enter an amount in string, for example 11.56: ");
+
+      String amount = input.nextLine();
+
+      /*int remainingAmount = (amount * 100);
+
+      //Find the number of one dollars
+      int numberOfOneDollars = remainingAmount / 100;
+
+      remainingAmount = remainingAmount % 100;
+
+      // Find the number of quarters in the remaining amount
+      int numberOfQuarters = remainingAmount % 25;
+
+      // Find the number of dimes in the remaining amount
+      int numberOfDimes = remainingAmount / 10;
+      remainingAmount = remainingAmount % 10;
+
+     // Find the number of nickels in the remaining amount
+      int numberOfNickels = remainingAmount / 5;
+
+      remainingAmount = remainingAmount % 5;
+
+      // Find the number of pennies in the remaining amount
+      int numberOfPennies = remainingAmount;
+
+      // Display results
+      System.out.println("Your amount " + amount + " consists of");
+      System.out.println(" " + numberOfOneDollars + " dollars");
+      System.out.println(" " + numberOfQuarters + " quarters ");
+      System.out.println(" " + numberOfDimes + " dimes");
+      System.out.println(" " + numberOfNickels + " nickels");
+      System.out.println(" " + numberOfPennies + " pennies");
+
+*/
+    }
+ }
+
